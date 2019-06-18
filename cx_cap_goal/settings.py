991512e77cd2190743a392b1b17025fc02ddce7a
Amy Mok <amy.mok@gsa.gov>
2019-06-18 11:35:29 -0700

    Cloud.gov setup, update deps, add invalid data

--- cx_cap_goal/settings.py
+++ cx_cap_goal/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,11 @@ SECRET_KEY = 'u6ovcmqeo*^xi_%p#1=mzr$qyk*-%d4)#h#gmva5kn4=*k0#u@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '.cloud.gov',
+]
 
 
 # Application definition
@@ -81,12 +86,7 @@ WSGI_APPLICATION = 'cx_cap_goal.wsgi.application'
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'cxcapgoaldb',
-    }
-}
+DATABASES = {'default': dj_database_url.config(default='postgres:///cxcapgoaldb')}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -123,7 +123,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 DATA_INGEST = {
